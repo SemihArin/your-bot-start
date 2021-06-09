d3015cdac58c99bf0d009637d99884efd62fe940
SemihArin <76950240+SemihArin@users.noreply.github.com>
2021-06-10 00:04:39 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.on("ready", () =>{
+ console.log("Bot aktif tutuluyor  : " +client.user.username) 
+}) 
+client.login("ODQ1NzQzMDM4MzkxMTIzOTk4.YKlZlQ.8io63TlMLBV0yMKqwJqcghH8FkI") 
