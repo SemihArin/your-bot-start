6cc168eaf770883fc0c2e4512188c2c7789ed03c
SemihArin <76950240+SemihArin@users.noreply.github.com>
2021-08-28 11:04:33 +0300

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-client.on("ready", () =>{
- console.log("Bot aktif tutuluyor  : " +client.user.username)   
-})
-client.login("") 
